e1d8bfb9ef4ff3417872528fd514fddf591104c0
Ted Tran <tedtran2019@gmail.com>
2022-10-07 22:50:44 -0700

    Completed project 04, MULT and FILL asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,80 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+  @SCREEN
+  D = A
+  @screen
+  M = D
+
+  @256
+  D = A
+  @y
+  M = D
+
+  @KBD
+  D = M
+  @kbd
+  M = D
+  D = M
+
+  @YLOOP2
+  D;JEQ
+
+  (YLOOP)
+    @32
+    D = A
+    @x
+    M = D
+
+    @y
+    M = M - 1
+    D = M
+    @LOOP
+    D;JLT
+
+    (XLOOP)
+      @screen
+      A = M
+      M = -1
+
+      @screen
+      M = M + 1
+
+      @x
+      M = M - 1
+      D = M
+      @XLOOP
+      D;JGT
+
+    @YLOOP
+    0;JMP
+
+  (YLOOP2)
+    @32
+    D = A
+    @x
+    M = D
+
+    @y
+    M = M - 1
+    D = M
+    @LOOP
+    D;JLT
+
+    (XLOOP2)
+      @screen
+      A = M
+      M = 0
+
+      @screen
+      M = M + 1
+
+      @x
+      M = M - 1
+      D = M
+      @XLOOP2
+      D;JGT
+
+    @YLOOP2
+    0;JMP
